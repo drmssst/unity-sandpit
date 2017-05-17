147328fbcb79d9ed299bf597060ca2374a9bf53f
Shaun Tinline <stjus@live.com>
2017-05-16 20:53:27 -0700

    Brick laying achieved; seems to include imported FPS package

---
+++ BrickLaying/Assets/scripts/BoxController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour {
+
+    private Rigidbody box;
+
+	// Use this for initialization
+	void Start () {
+        box = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+        box.AddForce(force * 10);
+    }
+}
---
+++ BrickLaying/Assets/scripts/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public Transform onHand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = onHand.position;
+        this.transform.parent = GameObject.Find("FPSController").transform;
+        this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
